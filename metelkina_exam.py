105bee7f35f229e4e83194168fd7a7914a1a6d84
Таисия Метелкина < >
2016-06-29 14:08:40 +0300

    my exam

--- metelkina_exam.py
+++ metelkina_exam.py
@@ -1,3 +1,30 @@
 #Метелкина Таисия, БКЛ-153.
-#Вариант .
+import re
 
+name = 'new.txt'
+
+def first(name): 
+    f = open (name, 'r', encoding = 'utf-8')
+    string = f.read()
+    f.close()
+    regex = '[А-ЯЁ]\. [А-ЯЁ][А-ЯЁа-яё-]+' 
+    name_1 = re.findall (regex, string)
+    if name != None:
+        for element in name_1:
+            print (element) # 1 задание
+            
+def second (name):
+    f = open (name, 'r', encoding = 'utf-8')
+    string = f.read()
+    f.close()
+    name_2 = re.findall ('[А-ЯЁ](?:\.|[а-яё-]+)(?: )?(?:[А-ЯЁ]\. )?[А-ЯЁ][А-Яа-яё-]+', string)
+    if name_2 != None:
+        for element in name_2:
+            print (element) #2 задание
+
+def main():
+    val1 = first (name)
+    val2 = second (name)
+    
+if __name__ == '__main__' :
+    main()
